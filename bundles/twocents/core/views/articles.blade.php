c14b1e07608ccdbb408b821629d32d5d18a090b4
Jason Lewis <jason.lewis1991@gmail.com>
2012-11-06 01:55:31 +1100

    Updating the theme.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- bundles/twocents/core/views/articles.blade.php
+++ bundles/twocents/core/views/articles.blade.php
@@ -18,44 +18,12 @@
 		@endif
 	</div>
 
-	<div class="article">
-		<h2 class="title"><a href="{{ URL::to_route('twocents: article', array($article->slug)) }}">{{ $article->title }}</a></h2>
-
-		<div class="meta">
-			By {{ $article->author->name }} on <span class="date">{{ date('j F, Y', strtotime($article->date)) }}</span>
-		</div>
-
-		<div class="body">
-			{{ $article->intro }}
-		</body>
-
-		@if($article->more)
-			{{ HTML::link_to_route('twocents: article', 'Read more', array($article->slug), array('class' => 'read-more')) }}
-		@endif
-	</div>
-
 	<div class="spacer"></div>
-
-	<div class="article">
-		<h3 class="title"><a href="{{ URL::to_route('twocents: article', array($article->slug)) }}">{{ $article->title }}</a></h3>
-
-		<div class="meta">
-			By {{ $article->author->name }} on <span class="date">{{ date('j F, Y', strtotime($article->date)) }}</span>
-		</div>
-	</div>
-
-	<div class="article">
-		<h3 class="title"><a href="{{ URL::to_route('twocents: article', array($article->slug)) }}">{{ $article->title }}</a></h3>
-
-		<div class="meta">
-			By {{ $article->author->name }} on <span class="date">{{ date('j F, Y', strtotime($article->date)) }}</span>
-		</div>
-	</div>
 
 	@foreach($articles as $article)
 
 	<div class="article">
-		<h3 class="title"><a href="{{ URL::to_route('twocents: article', array($article->slug)) }}">{{ $article->title }}</a></h2>
+		<h3 class="title"><a href="{{ URL::to_route('twocents: article', array($article->slug)) }}">{{ $article->title }}</a></h3>
 
 		<div class="meta">
 			By {{ $article->author->name }} on <span class="date">{{ date('j F, Y', strtotime($article->date)) }}</span>
