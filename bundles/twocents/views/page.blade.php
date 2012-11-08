ebf7c81772bf0e852b21c0975c8eac62d811af2c
Jason Lewis <jason.lewis1991@gmail.com>
2012-11-08 20:42:31 +1100

    Forgot to wrap in articles div.
    Signed-off-by: Jason Lewis <jason.lewis1991@gmail.com>

--- bundles/twocents/views/page.blade.php
+++ bundles/twocents/views/page.blade.php
@@ -1,9 +1,11 @@
-<div class="article">
-	@if($page->title)
-		<h2 class="title">{{ $page->title }}</h2>
-	@endif
+<div id="articles">
+	<div class="article">
+		@if($page->title)
+			<h2 class="title">{{ $page->title }}</h2>
+		@endif
 
-	<div class="body">
-		{{ $page->body }}
+		<div class="body">
+			{{ $page->body }}
+		</div>
 	</div>
 </div>
